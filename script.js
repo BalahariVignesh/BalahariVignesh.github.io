41f4d31a4ddad3420a3e1698a0c1512d3d00bbdd
Balahari Vignesh Balu <11013369@stud.hochschule-heidelberg.de>
2020-10-18 10:27:59 +0200

    test commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for(var i=0; themeDots.length>i;i++){
     themeDots[i].addEventListener('click', function(){
         let mode = this.dataset.mode
-        console.log('Option Clicked',mode)
+        console.log('Option Clicked:',mode)
         setTheme(mode)
     })
 }
